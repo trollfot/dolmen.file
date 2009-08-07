8a0842e0c0ac997d59e546bdea287837c3492694
Souheil CHELFOUH <trollfot@vittra.org>
2009-08-07 13:42:22 +0200

    Corrected dependencies

--- src/dolmen/file/__init__.py
+++ src/dolmen/file/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from field import FileField, IFileField
-from widget import FileWidgetMixin, FileDownloadWidget, FileUploadWidget
 from access import FilePublisher, FileTraverser, DownloadTraverser
 from _persistent import NamedFile, INamedFile
 from _property import FileProperty
