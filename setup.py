3c2bb05ee32af646815e2eb9b9c1ec1b62700aa8
Souheil CHELFOUH <trollfot@vittra.org>
2009-10-23 15:27:35 +0200

    Bumping version 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from os.path import join
 from setuptools import setup, find_packages
 
 name = 'dolmen.file'
-version = '0.3'
+version = '0.4dev'
 readme = open(join('src', 'dolmen', 'file', "README.txt")).read()
 history = open(join('docs', 'HISTORY.txt')).read()
 
