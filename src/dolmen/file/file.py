286d41262488e64266752577bb451b3364c47d86
Souheil CHELFOUH <trollfot@vittra.org>
2009-10-23 15:21:56 +0200

    Changed the filename cleaning method, now exposed in the public API

--- src/dolmen/file/file.py
+++ src/dolmen/file/file.py
@@ -1,18 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import zope.app.file
 from zope.schema import TextLine
-from zope.schema.fieldproperty import FieldProperty
 from zope.interface import implements
 from zope.contenttype import guess_content_type
-from zope.app.file import File
-from zope.app.file.interfaces import IFile
+from zope.schema.fieldproperty import FieldProperty
+
+from dolmen.file import clean_filename
 
 
-def cleanupFileName(filename):
-    return unicode(filename.split('\\')[-1].split('/')[-1])
-
-
-class INamedFile(IFile):
+class INamedFile(zope.app.file.interfaces.IFile):
     """Defines a file that is aware of its filename.
     """
     filename = TextLine(
@@ -22,7 +19,7 @@ class INamedFile(IFile):
         )
 
 
-class NamedFile(File):
+class NamedFile(zope.app.file.file.File):
     """A simple INamedFile implementation that can guess the content type
     from the value and the filename.
     """
@@ -32,7 +29,7 @@ class NamedFile(File):
     def __init__(self, data='', contentType='', filename=None):
         self.data = data
         if filename is not None:
-            self.filename = cleanupFileName(filename)
+            self.filename = clean_filename(filename)
         if not contentType and filename:
             # If we handle large files, we don't want them read just
             # to guess the content type. We provide only the filename.
